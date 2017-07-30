5ebf957f787fba4fc773e745614a0c9d3522a488
Achint Sandhu <sandhu@7theta.com>
2017-07-30 11:50:43 -0400

    Upgrade via dependency

--- project.clj
+++ project.clj
@@ -8,12 +8,12 @@
 ;;   the terms of this license.
 ;;   You must not remove this notice, or any others, from this software.
 
-(defproject com.7theta/re-frame-via-fx "0.2.0"
+(defproject com.7theta/re-frame-via-fx "0.2.2"
   :description "A re-frame effects handler for via WebSocket based messaging"
   :url "https://github.com/7theta/re-frame-via-fx"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[com.7theta/via "0.4.0"]
-                 [re-frame "0.9.2"]]
+  :dependencies [[com.7theta/via "0.5.0"]
+                 [re-frame "0.9.4"]]
   :scm {:name "git"
         :url "https://github.com/7theta/re-frame-via-fx"})
